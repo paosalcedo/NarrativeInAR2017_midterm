7ea6ccd3c596e3135d976f0ace76d79166d123af
DESKTOP-AV91D2I\paosa <paosalcedo@gmail.com>
2017-10-10 20:50:53 -0400

    Added DOTween to help with animations; added Paper.cs and GazeControl.cs

---
+++ AR Demo/Assets/Scripts/GazeControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeControl : MonoBehaviour {
+
+	private float gazeTime = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ShootRay();
+	}
+
+	public void ShootRay(){
+	
+		Ray ray = new Ray(transform.position, transform.forward);
+		
+		RaycastHit rayHit = new RaycastHit();
+
+		if(Physics.Raycast (ray, out rayHit, Mathf.Infinity)){
+ 			if(rayHit.transform.GetComponent<Paper>() != null){ 
+				gazeTime += Time.deltaTime;
+				if(gazeTime >= 3f){
+					//trigger something.
+					Debug.Log("Lol!");
+				}
+			}
+		}
+	}
+}
